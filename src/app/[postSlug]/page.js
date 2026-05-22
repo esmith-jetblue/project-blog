444fde2024f64e6ac43dbd08ddcac46145293a1d
Elijah Smith <esmith@jetblue.com>
2026-05-22 17:07:10 -0500

    Exercise 3

--- src/app/[postSlug]/page.js
+++ src/app/[postSlug]/page.js
@@ -5,6 +5,19 @@ import BlogHero from "@/components/BlogHero";
 import styles from "./postSlug.module.css";
 import { loadBlogPost } from "@/helpers/file-helpers";
 import { MDXRemote } from "next-mdx-remote/rsc";
+import { BLOG_TITLE } from "@/constants";
+
+export async function generateMetadata({ params }) {
+  const { postSlug } = await params;
+  const {
+    frontmatter: { title, abstract },
+  } = await loadBlogPost(postSlug);
+
+  return {
+    title: `${title} • ${BLOG_TITLE}`,
+    description: abstract,
+  };
+}
 
 async function BlogPost({ params }) {
   const { postSlug } = await params;
