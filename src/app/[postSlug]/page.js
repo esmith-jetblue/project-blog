aa780ed666ee2109daf140bc58977eebfa6efd73
Elijah Smith <esmith@jetblue.com>
2026-05-27 16:56:25 -0500

    Exercise 5A

--- src/app/[postSlug]/page.js
+++ src/app/[postSlug]/page.js
@@ -2,11 +2,16 @@ import React from "react";
 
 import BlogHero from "@/components/BlogHero";
 import CodeSnippet from "@/components/CodeSnippet";
+import dynamic from "next/dynamic";
 
 import styles from "./postSlug.module.css";
 import { loadBlogPost } from "@/helpers/file-helpers";
 import { MDXRemote } from "next-mdx-remote/rsc";
 import { BLOG_TITLE } from "@/constants";
+
+const DivisionGroupsDemo = dynamic(() =>
+  import("@/components/DivisionGroupsDemo")
+);
 
 export async function generateMetadata({ params }) {
   const { postSlug } = await params;
@@ -31,7 +36,10 @@ async function BlogPost({ params }) {
     <article className={styles.wrapper}>
       <BlogHero title={title} publishedOn={publishedOn} />
       <div className={styles.page}>
-        <MDXRemote source={content} components={{ pre: CodeSnippet }} />
+        <MDXRemote
+          source={content}
+          components={{ pre: CodeSnippet, DivisionGroupsDemo }}
+        />
       </div>
     </article>
   );
